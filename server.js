9e81d8efa394cce09026631ca490922d571e5e53
stephen <stephenmurena@gmail.com>
2017-05-26 14:06:14 -0700

    server running, rendering index

--- server.js
+++ server.js
@@ -1,39 +1,46 @@
 //dependencies
 const express = require('express');
-const path = require('path');
 const bodyParser = require('body-parser');
-const cheerio = require('cheerio');
-const mongoose = require('mongoose');
-const request = require('request');
-//may need methodOverride, verify before deployment
+const logger = require("morgan");
+const methodOverride = require('method-override');
+const exphbs = require("express-handlebars");
+const path = require("path");
 
-// Mongoose mpromise deprecated - using bluebird promises
-let Promise = require('bluebird');
 
-//const for application
+//const for application, initialize express
 const app = express();
-const port = process.env.PORT || '3000';
-const config = require('./config');
-const handlebars = require('./config/handlebars');
+const PORT = process.env.PORT || '3000';
+app.listen(PORT, function(err, res) {
+	if(err) throw err;
+	console.log("App running on Port:" + PORT);
+})
+
 // let db = require('./app/models');
 
-
-//views
-app.set('views', path.join(__dirname, 'app/views'));
-app.set('view engine', 'hbs');
-
 //body-parser
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
+//method override
+app.use(methodOverride("_method"));
+
+//views
+// app.set('views', path.join(__dirname, 'app', 'views'));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 //public- static files (if any)
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use(express.static("public"));
 
 
 //routes
-app.use('/', require('.app/routes/html'));
+let routes = require('./routes/html');
+// app.use('/', routes);
+app.get('/', function(req, res) {
+	res.render("index");
+})
 //additional routes may follow;
 
 //error verify useage, neccessary for this type of app?
