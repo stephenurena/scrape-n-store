896cd1fbff5356668fcae8bff02c961783ba5df8
stephen <stephenmurena@gmail.com>
2017-05-27 01:24:28 -0700

    push

--- config/handlebars.js
+++
@@ -1,7 +0,0 @@
-const handlebars = require('hbs');
-const config = require('./index');
-const path = require('path');
-
-handlebars.registerPartials(path.join(config.root, '/app/views/partials'));
-
-module.exports = handlebars;
